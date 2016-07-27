6788d5ea39cab0557c6470fb845f0f2cc8431e79
sandpiturtle <nik96lip@mail.ru>
2016-07-27 12:44:02 +0300

    started SmallObjAllocator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
+    std::vector<int> v = { 1, 2, 3 };
+    std::vector<int>::iterator i = v.begin();
+    std::cout << (--i > v.begin());
+    for (int j = 0; j < 100; j++) {
+        --i;
+    }
+    i = v.end();
+    for (int j = 0; j < 100; j++) {
+        ++i;
+    }
+    std::cout << (--i > v.begin());
     return 0;
 }
